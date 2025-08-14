92a459949e5cddd74851fc4b92bd5871addb8531
atp-gh <atp@disroot.org>
2025-08-14 19:26:37 +0800

    fix: Fix the daed config

--- machines/icecream/services/daed.nix
+++ machines/icecream/services/daed.nix
@@ -3,7 +3,8 @@
     inputs.daeuniverse.nixosModules.dae
     inputs.daeuniverse.nixosModules.daed
   ];
-  services.dae = {
+  services.daed = {
     enable = true;
+    listen = "0.0.0.0:2023";
   };
 }
