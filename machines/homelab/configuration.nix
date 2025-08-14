c83b5a0847f13035e59442a501e29dc9ea68fe5b
atp-gh <atp@disroot.org>
2025-08-15 05:00:21 +0800

    chore: Shut down some useless services

--- machines/homelab/configuration.nix
+++ machines/homelab/configuration.nix
@@ -15,9 +15,8 @@
       ../../modules/services/ssh.nix
       ../../modules/services/tailscale.nix
 
-      # ./services/alist.nix
       ./services/aria2.nix
-      ./services/cockpit.nix
+      # ./services/cockpit.nix
       # ./services/code-server.nix
       ./services/dashy.nix
       ./services/forgejo.nix
@@ -25,12 +24,11 @@
       ./services/gotify.nix
       # ./services/home-assistant.nix
       ./services/homebox.nix
-      ./services/immich.nix
+      # ./services/immich.nix
       ./services/iperf3.nix
       # ./services/jellyfin.nix
       # ./services/kodi.nix
       ./services/nginx.nix
-      ./services/podman.nix
       ./services/openlist.nix
       # ./services/podman.nix
       ./services/postgres.nix
@@ -41,7 +39,7 @@
       ./services/syncthing.nix
       ./services/wakapi.nix
 
-      ./services/docker/romm.nix
+      # ./services/docker/romm.nix
     ]
     ++ lib.filesystem.listFilesRecursive ../../modules/options;
   boot = {
