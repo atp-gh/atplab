584374e5a3c0f480be216dddf3efd2c3a326cd81
atp-gh <atp@disroot.org>
2025-08-24 02:14:24 +0800

    fix: Fix some backup paths

--- machines/homelab/services/restic.nix
+++ machines/homelab/services/restic.nix
@@ -5,44 +5,42 @@
 }: {
   services.restic = {
     backups = {
-      local = {
-        initialize = true;
-        passwordFile = config.sops.secrets.homelab-restic-local-password.path;
-        paths = [
-          "/var/lib/alist"
-          "/var/lib/forgejo"
-          "/var/lib/homebox"
-          "/var/lib/jellyfin"
-          "/var/lib/postgresql"
-          "/var/lib/wakapi"
-        ];
-        repository = "/backup/restic";
-        timerConfig = {
-          OnCalendar = "21:00:00";
-          Persistent = true;
-        };
-        backupPrepareCommand = ''
-          ${pkgs.curl}/bin/curl "http://127.0.0.1:1245/message?token=AkFEQLUQdbIP7yG" \
-            -F "title=restic local backup start" \
-            -F "message=$(date "+%H:%M:%S")" \
-            -F "priority=2"
-        '';
-        backupCleanupCommand = ''
-          ${pkgs.curl}/bin/curl "http://127.0.0.1:1245/message?token=AkFEQLUQdbIP7yG" \
-            -F "title=restic local logs" \
-            -F "message=$(journalctl -u restic-backups-local.service --since '10 minute ago' -o cat) | over." \
-            -F "priority=1"
-        '';
-      };
+      # local = {
+      #   initialize = true;
+      #   passwordFile = config.sops.secrets.homelab-restic-local-password.path;
+      #   paths = [
+      #     "/var/lib/forgejo"
+      #     "/var/lib/homebox"
+      #     "/var/lib/openlist"
+      #     "/var/lib/postgresql"
+      #     "/var/lib/wakapi"
+      #   ];
+      #   repository = "/backup/restic";
+      #   timerConfig = {
+      #     OnCalendar = "21:00:00";
+      #     Persistent = true;
+      #   };
+      #   backupPrepareCommand = ''
+      #     ${pkgs.curl}/bin/curl "http://127.0.0.1:1245/message?token=AkFEQLUQdbIP7yG" \
+      #       -F "title=restic local backup start" \
+      #       -F "message=$(date "+%H:%M:%S")" \
+      #       -F "priority=2"
+      #   '';
+      #   backupCleanupCommand = ''
+      #     ${pkgs.curl}/bin/curl "http://127.0.0.1:1245/message?token=AkFEQLUQdbIP7yG" \
+      #       -F "title=restic local logs" \
+      #       -F "message=$(journalctl -u restic-backups-local.service --since '10 minute ago' -o cat) | over." \
+      #       -F "priority=1"
+      #   '';
+      # };
       remote1 = {
         initialize = true;
         passwordFile = config.sops.secrets.homelab-restic-remote1-password.path;
         environmentFile = config.sops.secrets.homelab-restic-remote1-environment.path;
         paths = [
-          "/var/lib/alist"
           "/var/lib/forgejo"
           "/var/lib/homebox"
-          "/var/lib/jellyfin"
+          "/var/lib/openlist"
           "/var/lib/postgresql"
           "/var/lib/wakapi"
         ];
