3f7aa5e8e401818f02d720feefc62155c37110c9
atp-gh <atp@disroot.org>
2025-08-24 02:13:38 +0800

    fix: Make syncthing delicious

--- machines/homelab/services/syncthing.nix
+++ machines/homelab/services/syncthing.nix
@@ -2,27 +2,26 @@ _: {
   services = {
     syncthing = {
       enable = true;
-      dataDir = "/backup";
       user = "atp";
       group = "users";
       openDefaultPorts = true;
       guiAddress = "localhost:8384";
       settings = {
-        # devices = {
-        #   asusbook = {
-        #     id = "PNXRTAP-4SZD2AF-KHARWWZ-6YBM4P6-XUV2AN2-2HEAUFQ-LY7Z2UM-JANGGQV";
-        #   };
-        #   PostmarketOS = {
-        #     id = "OP5TTKB-AI2JABB-6YBI2Q4-XHHFAQQ-QHO23VV-5QZBMXW-5MAM42F-BX74AQZ";
-        #   };
-        # };
+        devices = {
+          asusbook = {
+            id = "PNXRTAP-4SZD2AF-KHARWWZ-6YBM4P6-XUV2AN2-2HEAUFQ-LY7Z2UM-JANGGQV";
+          };
+          # PostmarketOS = {
+          #   id = "OP5TTKB-AI2JABB-6YBI2Q4-XHHFAQQ-QHO23VV-5QZBMXW-5MAM42F-BX74AQZ";
+          # };
+        };
         folders = {
-          "/backup/sync" = {
+          "/var/lib/Sync" = {
             id = "default";
-            # devices = [
-            #   "asusbook"
-            #   "PostmarketOS"
-            # ];
+            devices = [
+              "asusbook"
+              # "PostmarketOS"
+            ];
             versioning.type = "trashcan";
           };
         };
