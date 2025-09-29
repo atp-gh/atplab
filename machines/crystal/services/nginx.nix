e3e9f49a2b08cfe1f49a9017114cebdcb7b674a6
atp-gh <atp@disroot.org>
2025-09-29 14:45:04 +0800

    style: Delete useless comments

--- machines/crystal/services/nginx.nix
+++ machines/crystal/services/nginx.nix
@@ -23,39 +23,6 @@
     enable = true;
     streamConfig = import ../../../sops/eval/crystal/nginx-stream-config.nix;
     virtualHosts = {
-      # "wakapi.0pt.dpdns.org" = {
-      #   forceSSL = true;
-      #   useACMEHost = "0pt.dpdns.org";
-      #   locations."/" = {
-      #     proxyPass = "http://127.0.0.1:3001";
-      #     recommendedProxySettings = true;
-      #     # extraConfig = ''
-      #     #   proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
-      #     # '';
-      #   };
-      # };
-      # "hs.0pt.dpdns.org" = {
-      #   forceSSL = true;
-      #   useACMEHost = "0pt.dpdns.org";
-      #   locations."/" = {
-      #     root = "/var/www/html/reverse-proxy-test";
-      #     index = "index.html";
-      #   };
-      # };
-      # "hs.0pt.dpdns.org" = {
-      #   forceSSL = true;
-      #   sslCertificate = "/var/lib/cf-cert/example1.com.pem";
-      #   sslCertificateKey = "/var/lib/cf-cert/example1.com.key";
-      #   locations."/" = {
-      #     proxyPass = "http://127.0.0.1:8080";
-      #     proxyWebsockets = true;
-      #     recommendedProxySettings = true;
-      #     extraConfig = ''
-      #       proxy_buffering off;
-      #       add_header Strict-Transport-Security "max-age=15552000; includeSubDomains" always;
-      #     '';
-      #   };
-      # };
       "backup1.lkt.icu" = {
         forceSSL = true;
         sslCertificate = "/var/lib/cf-cert/example.com.pem";
@@ -70,32 +37,6 @@
           '';
         };
       };
-      # "garage.0pt.im" = {
-      #   forceSSL = true;
-      #   sslCertificate = "/var/0pt.im.pem";
-      #   sslCertificateKey = "/var/0pt.im.key";
-      #   locations."/" = {
-      #     proxyPass = "http://127.0.0.1:3900";
-      #     proxyWebsockets = true;
-      #     recommendedProxySettings = true;
-      #     extraConfig = ''
-      #       proxy_buffering off;
-      #     '';
-      #   };
-      # };
-      # "garageweb.0pt.im" = {
-      #   forceSSL = true;
-      #   sslCertificate = "/var/0pt.im.pem";
-      #   sslCertificateKey = "/var/0pt.im.key";
-      #   locations."/" = {
-      #     proxyPass = "http://127.0.0.1:3902";
-      #     # proxyWebsockets = true;
-      #     recommendedProxySettings = true;
-      #     extraConfig = ''
-      #       proxy_buffering off;
-      #     '';
-      #   };
-      # };
     };
   };
   networking.firewall = {
