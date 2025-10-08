cf9e3cdd05c5a0f421ac2157f64d69a912d82195
atp-gh <atp@disroot.org>
2025-10-08 22:09:28 +0800

    test: Try to fix ipv6 problem

--- machines/crystal/network.nix
+++ machines/crystal/network.nix
@@ -11,23 +11,23 @@ _: {
       address = import ../../sops/eval/crystal/ipv4-gateway.nix;
       interface = "eth0";
     };
-    interfaces.eth0.ipv6.addresses = [
-      {
-        address = import ../../sops/eval/crystal/ipv6-address1.nix;
-        prefixLength = import ../../sops/eval/crystal/ipv6-prefix.nix;
-      }
-      {
-        address = import ../../sops/eval/crystal/ipv6-address2.nix;
-        prefixLength = import ../../sops/eval/crystal/ipv6-prefix.nix;
-      }
-      {
-        address = import ../../sops/eval/crystal/ipv6-address3.nix;
-        prefixLength = import ../../sops/eval/crystal/ipv6-prefix.nix;
-      }
-    ];
-    defaultGateway6 = {
-      address = import ../../sops/eval/crystal/ipv6-gateway.nix;
-      interface = "eth0";
-    };
+    # interfaces.eth0.ipv6.addresses = [
+    #   {
+    #     address = import ../../sops/eval/crystal/ipv6-address1.nix;
+    #     prefixLength = import ../../sops/eval/crystal/ipv6-prefix.nix;
+    #   }
+    #   {
+    #     address = import ../../sops/eval/crystal/ipv6-address2.nix;
+    #     prefixLength = import ../../sops/eval/crystal/ipv6-prefix.nix;
+    #   }
+    #   {
+    #     address = import ../../sops/eval/crystal/ipv6-address3.nix;
+    #     prefixLength = import ../../sops/eval/crystal/ipv6-prefix.nix;
+    #   }
+    # ];
+    # defaultGateway6 = {
+    #   address = import ../../sops/eval/crystal/ipv6-gateway.nix;
+    #   interface = "eth0";
+    # };
   };
 }
