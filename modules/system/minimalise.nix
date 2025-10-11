991f68cee92a3a1902eea18af30176e9fbdb2473
atp-gh <atp@disroot.org>
2025-10-11 21:24:35 +0800

    rebuild: Update minimalise

--- modules/system/minimalise.nix
+++ modules/system/minimalise.nix
@@ -1,38 +1,43 @@
-{lib, ...}: {
+{lib, ...}:
+with lib; {
   # Minimize boot
   boot = {
-    bcache.enable = lib.mkForce false;
-    binfmt.addEmulatedSystemsToNixSandbox = lib.mkForce false;
+    bcache.enable = mkForce false;
+    binfmt.addEmulatedSystemsToNixSandbox = mkForce false;
     initrd = {
       checkJournalingFS = false;
     };
   };
 
   # Disable unnecessary documentation
-  documentation.enable = lib.mkForce false;
-  documentation.doc.enable = lib.mkForce false;
-  documentation.info.enable = lib.mkForce false;
-  documentation.man.enable = lib.mkForce false;
-  documentation.nixos.enable = lib.mkForce false;
+  documentation.enable = mkForce false;
+  documentation.doc.enable = mkForce false;
+  documentation.info.enable = mkForce false;
+  documentation.man.enable = mkForce false;
+  documentation.nixos.enable = mkForce false;
 
   # Minimize environment
   environment = {
-    defaultPackages = lib.mkForce [];
+    defaultPackages = mkForce [];
   };
 
   # Disable unnecessary fonts
-  fonts.fontconfig.enable = lib.mkForce false;
+  fonts.fontconfig.enable = mkForce false;
 
   # Disable unnecessary programs
   programs = {
-    command-not-found.enable = lib.mkForce false;
-    nano.enable = lib.mkForce false;
+    bash = {
+      completion.enable = mkForce false;
+      enableLsColors = mkForce false;
+    };
+    command-not-found.enable = mkForce false;
+    nano.enable = mkForce false;
   };
 
-  #Disable security features
+  # Disable security features
   security = {
-    pam.services.su.forwardXAuth = lib.mkForce false;
-    sudo.enable = lib.mkForce false;
+    pam.services.su.forwardXAuth = mkForce false;
+    sudo.enable = mkForce false;
   };
 
   # Minimize journal
@@ -51,37 +56,37 @@
 
   # Minimize services
   services = {
-    fstrim.enable = lib.mkForce false;
-    logrotate.enable = lib.mkForce false;
-    #nscd.enable = lib.mkForce false; # Disable this will make system.nssModules can not enable
-    resolved.enable = lib.mkForce false;
-    #timesyncd.enable = lib.mkForce false; # Disable this may cause wrong time
+    fstrim.enable = mkForce false;
+    logrotate.enable = mkForce false;
+    nscd.enable = mkForce false; # Disable this will make system.nssModules can not enable
+    resolved.enable = mkForce false;
+    timesyncd.enable = mkForce false;
   };
 
   # Minimize systemd services
   systemd = {
-    coredump.enable = lib.mkForce false;
-    enableEmergencyMode = lib.mkForce false;
-    oomd.enable = lib.mkForce false;
+    coredump.enable = mkForce false;
+    enableEmergencyMode = mkForce false;
+    oomd.enable = mkForce false;
     services = {
-      mount-pstore.enable = lib.mkForce false;
-      systemd-journal-flush.enable = lib.mkForce false;
-      systemd-pstore.enable = lib.mkForce false;
-      systemd-udev-settle.enable = lib.mkForce false;
-      systemd-update-utmp.enable = lib.mkForce false;
-      systemd-user-sessions.enable = lib.mkDefault false;
+      mount-pstore.enable = mkForce false;
+      systemd-journal-flush.enable = mkForce false;
+      systemd-pstore.enable = mkForce false;
+      systemd-udev-settle.enable = mkForce false;
+      systemd-update-utmp.enable = mkForce false;
+      systemd-user-sessions.enable = mkForce false;
     };
   };
 
   # Disable NSS modules
-  #system.nssModules = lib.mkForce []; # Uncomment this will make cockpit can not run
+  system.nssModules = mkForce []; # This will make cockpit can not run
 
   # Disable desktop environment related services
   xdg = {
-    autostart.enable = lib.mkDefault false;
-    icons.enable = lib.mkDefault false;
-    menus.enable = lib.mkDefault false;
-    mime.enable = lib.mkDefault false;
-    sounds.enable = lib.mkDefault false;
+    autostart.enable = mkForce false;
+    icons.enable = mkForce false;
+    menus.enable = mkForce false;
+    mime.enable = mkForce false;
+    sounds.enable = mkForce false;
   };
 }
