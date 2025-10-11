3da71c4d4bdc4e95f9b1381b75141d28b2c85804
atp-gh <atp@disroot.org>
2025-10-11 21:28:30 +0800

    rebuild: Rename and delete useless config

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -79,39 +79,4 @@ with lib; {
     ];
     tmp.cleanOnBoot = true;
   };
-
-  console.keyMap = "us";
-
-  # Set your time zone.
-  time = {
-    timeZone = "Asia/Singapore";
-    hardwareClockInLocalTime = false;
-  };
-
-  # Select internationalisation properties.
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
-  };
-
-  networking = {
-    nftables.enable = true;
-    firewall = {
-      allowedTCPPorts = [
-        222
-        443
-      ];
-    };
-  };
 }
