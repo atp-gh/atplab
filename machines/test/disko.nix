593216c53b5cd4181e75c98d332d2c9071b6df77
atp-gh <atp@disroot.org>
2025-10-11 19:35:24 +0800

    rebuild: Fix disko.nix

--- machines/test/disko.nix
+++ machines/test/disko.nix
@@ -1,27 +1,20 @@
-{
-  # swapDevices = [
-  #   {
-  #     label = "swapfile";
-  #     discardPolicy = "both";
-  #   }
-  # ];
+_: {
   disko.devices = {
     disk = {
       main = {
         type = "disk";
-        device = "/dev/disk/by-path/virtio-pci-0000:04:00.0";
         content = {
           type = "gpt";
           partitions = {
             boot = {
-              size = "1M";
-              type = "EF02"; # for grub MBR
-              priority = 0;
-            };
-            ESP = {
-              size = "512M";
-              type = "EF00";
               priority = 1;
+              size = "1M";
+              type = "EF02";
+            };
+            esp = {
+              priority = 2;
+              size = "256M";
+              type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
@@ -29,92 +22,21 @@
                 mountOptions = ["umask=0077"];
               };
             };
-            swap = {
-              label = "swapfile";
-              size = "8G";
-              priority = 2;
-              content = {
-                type = "swap";
-                discardPolicy = "both";
-                resumeDevice = true;
-              };
-            };
-            zfs = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "zroot";
-              };
-            };
-          };
-        };
-      };
-      ex1 = {
-        name = "ex1";
-        device = "/dev/disk/by-path/virtio-pci-0000:08:00.0";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
             root = {
               size = "100%";
               content = {
                 type = "filesystem";
-                format = "ext4";
-                mountpoint = "/ex1";
+                format = "f2fs";
+                mountpoint = "/";
+                extraArgs = [
+                  "-O"
+                  "extra_attr,inode_checksum,sb_checksum,compression"
+                ];
+                mountOptions = [
+                  "compress_algorithm=zstd:6,compress_chksum,atgc,gc_merge,lazytime,nodiscard"
+                ];
               };
             };
-          };
-        };
-      };
-    };
-    zpool = {
-      zroot = {
-        type = "zpool";
-        rootFsOptions = {
-          # https://wiki.archlinux.org/title/Install_Arch_Linux_on_ZFS
-          acltype = "posixacl";
-          atime = "off";
-          compression = "lz4";
-          mountpoint = "none";
-          xattr = "sa";
-        };
-        options.ashift = "12";
-
-        datasets = {
-          home = {
-            type = "zfs_fs";
-            mountpoint = "/home";
-            # Used by services.zfs.autoSnapshot options.
-            options = {
-              "com.sun:auto-snapshot" = "true";
-              mountpoint = "legacy";
-            };
-          };
-          nix = {
-            type = "zfs_fs";
-            mountpoint = "/nix";
-            options = {
-              "com.sun:auto-snapshot" = "false";
-              mountpoint = "legacy";
-            };
-          };
-          persist = {
-            type = "zfs_fs";
-            mountpoint = "/persist";
-            options = {
-              "com.sun:auto-snapshot" = "false";
-              mountpoint = "legacy";
-            };
-          };
-          root = {
-            type = "zfs_fs";
-            mountpoint = "/";
-            options = {
-              "com.sun:auto-snapshot" = "false";
-              mountpoint = "legacy";
-            };
-            postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot/root@blank$' || zfs snapshot zroot/root@blank";
           };
         };
       };
