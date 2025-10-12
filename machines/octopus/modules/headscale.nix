76b679c91792d848b6366b1bb0fbcf125626999d
atp-gh <atp@disroot.org>
2025-10-12 18:46:04 +0800

    feat: Enable headscale

---
+++ machines/octopus/modules/headscale.nix
@@ -0,0 +1,45 @@
+{config, ...}: let
+  cfg = config.services.headscale;
+in {
+  services = {
+    headscale = {
+      enable = true;
+      address = "127.0.0.1";
+      port = 8080;
+      settings = {
+        dns = {
+          nameservers.global = [
+            "127.0.0.1"
+            "::1"
+          ];
+          magic_dns = false;
+        };
+        server_url = "https://hs.0pt.dpdns.org";
+      };
+    };
+    nginx = {
+      virtualHosts = {
+        "hs.0pt.dpdns.org" = {
+          forceSSL = true;
+          kTLS = true;
+          sslCertificate = "/etc/nginx/self-sign.crt";
+          sslCertificateKey = "/etc/nginx/self-sign.key";
+          extraConfig = ''
+            proxy_hide_header X-Powered-By;
+            proxy_hide_header Server;
+          '';
+          locations = {
+            "/" = {
+              proxyPass = "http://${toString cfg.address}:${toString cfg.port}";
+              recommendedProxySettings = true;
+              proxyWebsockets = true;
+              extraConfig = ''
+                proxy_buffering off;
+              '';
+            };
+          };
+        };
+      };
+    };
+  };
+}
