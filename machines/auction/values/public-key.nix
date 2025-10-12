954efad79418bb52fbd7ce46aef441356c296233
atp-gh <atp@disroot.org>
2025-10-12 11:10:46 +0800

    rebuild: Forget to add public-key

---
+++ machines/auction/values/public-key.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:2uFArlsHV+ZX0iA7fYC1IE5seK54xq0=,iv:jdzS1q3a29ihNr93zSOul0PFWevjCFMa2KjlXg4PRog=,tag:KBKa7VkZG4bqvJtqTTZOuA==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1gym933jnyln9nud8vhg44vtp983prgfpnlxr0zw2tr9x9s575vyqyufmyd",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSA0dmpjVzVUV1ZBNG4vR3I1\nekRuMXlWdEl5blFveWM2d0tVcVVaMjdZQWc4Cm5pNjNNdnlkTWRablNxRVNjckxa\nUE0zeUN4ZTJtUXNuand0QzdGVEJMcWMKLS0tIFdWbjFrbkN0N0NOOHU3OFQwS1Rs\nUEhWaTRhTlNJcFVZaHBmNFE3TEhKWTQKnJgjihIpVY3GV2RhMM/YNb2XtXP3Yg2C\nkMd0h5UcmC0IVbm5KKlz7453XyQ0lqbJeMivDAfSZt+BdveRsvjDfA==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-10-12T03:09:56Z",
+		"mac": "ENC[AES256_GCM,data:z1UE3mqK/uD4lRc2EN1vPLwhldd02b3p+We4jgq23tDzMTm2TxZcELUQRj07/iNj03lC7Qqs+ij1U8zVnUjHioA/RfctDVARRzxWIvrDsvbrxdhqwfIyq0iZ8GE4pE8A6tsCnW6mLAgJpJLbbJTl/NPxpxcFsQtUhlsuyM0r7Mk=,iv:wys4PPyQFagthnNGk3xrKM/rA1lwFWAbYrCj9fY0Aa8=,tag:aLex5ld213a4UcB18H7YXg==,type:str]",
+		"version": "3.11.0"
+	}
+}
