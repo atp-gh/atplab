1b659eedc0a5ebc3b30f34a965d3df649e3054e6
atp-gh <atp@disroot.org>
2025-10-12 20:30:41 +0800

    fix: Fix komari-agent-rs version

--- pkgs/komari-agent-rs/default.nix
+++ pkgs/komari-agent-rs/default.nix
@@ -6,10 +6,10 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "komari-agent-rs";
-  version = "1.0.83";
+  version = "0.2.7";
   src = fetchurl {
     url = "https://github.com/GenshinMinecraft/komari-monitor-rs/releases/download/latest/komari-monitor-rs-linux-x86_64-musl";
-    sha256 = "sha256-99EcIjMtape2DynxpjkuzK0kceWVOHGR7njxuStSTf8=";
+    sha256 = "sha256-BMl6N51ZDqnIqKbPivAnyQWPxAVoXLs4jfU8zmfQY/4=";
   };
   dontUnpack = true;
   installPhase = ''
