3f1d9a8612d074949b72610c40778ad40210455b
atp-gh <atp@disroot.org>
2025-10-12 10:15:10 +0800

    rebuild: Migrate to example

--- machines/auction/services/komari-agent-rs.nix
+++
@@ -1,8 +0,0 @@
-_: {
-  services.komari-agent-rs = {
-    enable = true;
-    token = import ../../../sops/eval/auction/komari-token.nix;
-    endpoint = "https://eye.0pt.dpdns.org";
-    extraFlags = "-f 10";
-  };
-}
