edad4e5f7f0bb887c80c9351182b436ce763ed59
atp-gh <atp@disroot.org>
2025-10-12 10:13:51 +0800

    rebuild: Success to use sops secret for root password

--- machines/homelab/user.nix
+++ machines/homelab/user.nix
@@ -1,10 +1,14 @@
 {config, ...}: {
-  # sops.secrets.homelab-hashedPassword.neededForUsers = true;
+  sops.secrets.homelab-hashedPassword = {
+    format = "binary";
+    sopsFile = secrets/hashedPassword;
+    neededForUsers = true;
+  };
   users = {
     mutableUsers = false;
     users = {
       root = {
-        # hashedPasswordFile = config.sops.secrets.homelab-hashedPassword.path;
+        hashedPasswordFile = config.sops.secrets.homelab-hashedPassword.path;
         openssh.authorizedKeys.keys = [
           (import ../../modules/private/homelab/public-key.nix)
         ];
