6bbd551d2922f0e296cc7e64cf9e6e88d6a08961
atp-gh <atp@disroot.org>
2025-10-12 12:44:33 +0800

    rebuild: Forget import values and secrets

---
+++ machines/freezer/values/disko-main-device.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:DFQS+VdfXIlywjxPYAPcErva2J2498tKVx2Si/XXdx4lAeiTF9dCoyPPVlhv8n7BitW5Z8fgqVrXbxQ==,iv:rKah0qY1ELCAGLZMibSgCOqIkisFzd15YsYGKYtfPc=,tag:k8DcJUdSOUwInDgQbR64Q==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1gym933jnyln9nud8vhg44vtp983prgfpnlxr0zw2tr9x9s575vyqyufmyd",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cRpb24ub3JnL3YxCi0+IFgyNTUxOSBWdnl1Qkc0UUdhb2wvalZq\nQVYzNGljaXVqRGtCUzExQVRhbFlaVTZTNGdJCkFJeEBZjNrMjJaTm1UU2Z0bVlJ\nTWRkSjVtVmQ1NUmTXpZNS9yYkR4STAKLS0tIHJzZXo5ZXB0R2hFbkd3bXR2dGFE\nODZ1SlNIakZtRzhRODVyY1ByVFl5bjAX3YNwOua0rNZ4i67UXSb+EmwZBBT8m/2\nTSPfBemVh07A4me+1neE4VZTNgel5S39h+3b8GtR4p4FwudWJDow==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-10-12T04:42:46Z",
+		"mac": "ENC[AES256_GCM,data:fnL0RVrtnfRImsR0GYiQw0jOo/9Oaha7gkOFE42hyMB3w4ZOR4uiJKYlpkRo5HZcLUqn4V1W5bz5ciKbewh0AjWMa//5RBoDYZB57aLw4p9wADJbrx57ZqQT9y9vdCkvJLoDe/S08OerMREWUI2UfFKQMDL0HP6E9NLIpA0=,iv:YoadQz0FJnkJwHtHpuqjRoKtDzHwosiZn5JtklSfWU=,tag:1WX9/HioS6xmK+ClUS4A==,type:str]",
+		"version": "3.11.0"
+	}
+}
---
+++ machines/freezer/values/public-key.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:mpPUp3XWOgPZVYSqXWBfwCxQ+4TqwugFRUgGlDARiSBbt+FrvkjPY8mK5/6P4+FjL3VULJiNn3NlP8Q==,iv:5ATFzC/eC7PhnemZcYdm9SRKFALkgirdFEI2gLH/uw=,tag:BkOESRYShWBEocdUA7IPQ==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1gym933jnyln9nud8vhg44vtp983prgfpnlxr0zw2tr9x9s575vyqyufmyd",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5HRpb24ub3JnL3YxCi0+IFgyNTUxOSB0SFJ1d0dFeHB1Nkk1Vm5K\nYJXMW4yNENlQnlieC9pV2VVcHpUbVBpRFV3Cm9jNk1VcFVvRkgrb2xSbFcrYlJ1\nYVoN2Vtazd0MmVuQmpNSk1kSGFNZlkKLS0tIFVvdmYxZlRUWU9nT3FINjFlTWN4\nN1FjVnd3czBiY3lteHRveGxhSS9JUlEK/D+zJEqgRNA5ppPYG8g4wy9k2PRQPpa\nnrpL4jccpA8LW0RTr1ZLhNiUxvpK9mzfComr077Wi6tbzHqPiWrw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-10-12T04:42:46Z",
+		"mac": "ENC[AES256_GCM,data:PaJ5pg6wTJPfatu/VFsV/eCoAW7UiKK1UXWBptzrb0TPO/SXd1JDUxOMEa2810KKpNiR4XHO62DmPSZkL1j0aru8gJjKY9o8degodUC2qh3KAXjXbG2yBrKmqFmYhSWS7wWXMOBx/Ir4lkwBxYbO9PiuhBCd36C6CehtVU/A=,iv:w7WCyQ5Q/8MKhSWVqpLwsVd4TW5kGiEBhUPz634Fww=,tag:uJVd/Dh0XARgJzRetywiw==,type:str]",
+		"version": "3.11.0"
+	}
+}
