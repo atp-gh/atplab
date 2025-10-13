eca156d5fccffcceb5c44261843797bcc9a12c76
atp-gh <atp@disroot.org>
2025-10-13 12:15:17 +0800

    feat: Add squid hashed password

--- machines/squid/user.nix
+++ machines/squid/user.nix
@@ -1,14 +1,14 @@
 {config, ...}: {
-  # sops.secrets.squid-hashedPassword = {
-  #   format = "binary";
-  #   sopsFile = secrets/hashedPassword;
-  #   neededForUsers = true;
-  # };
+  sops.secrets.squid-hashedPassword = {
+    format = "binary";
+    sopsFile = secrets/hashedPassword;
+    neededForUsers = true;
+  };
   users = {
     mutableUsers = false;
     users = {
       root = {
-        # hashedPasswordFile = config.sops.secrets.squid-hashedPassword.path;
+        hashedPasswordFile = config.sops.secrets.squid-hashedPassword.path;
         openssh.authorizedKeys.keys = [
           (import values/public-key.nix)
         ];
