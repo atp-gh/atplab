df2b9748faf1192778c53070c5515b5c930efeb1
atp-gh <atp@disroot.org>
2025-10-14 19:56:45 +0800

    feat: Enable nginx

---
+++ machines/squid/values/self-sign-key.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:LWH3CEpllKpR1Ee40dmPHqjHM8NeaWbzYdZhe5fdrmNAXdtdxT8KdT1Ja7wjED7YAGiZf0HJJk4cxqj2K4dd61SKAWH2r1R0KbdAnff10rxM7j8mGVy3PjiA5MUUW2Z9KP5adFFFMttCqHjhXlXegzFjJQIiTNc/j04WR5CNsN3HAqfQNsdQufbcsO/CErWINA5vSjOmPD241bgq0mUFlj26HCZOOkfGHzaLknOiU3FwI+buAI/QMTyLjBBj97k42Kvo6L1aqbh33i62voRKfcPfSLqrxyn8SOVYmJ18yYf7VlXftUifq2U+8V/77R9B5JwkJyzIEC9mAl5p0x3/s5BRxgDaRuOB0SGlflKjHRthRjsqwtHzyBObk4M6eF3oekGWRJ9TbTTrIdrH0yRybv+E8d5TyDgxcukAioAVangM+uFh,iv:swsZTndYeT+nMIZGBYUHYHF3/w7NVh2woiUcNCLl+kk=,tag:e3IM3ZPDKMtg3ECFR66eBQ==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1gym933jnyln9nud8vhg44vtp983prgfpnlxr0zw2tr9x9s575vyqyufmyd",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBsV0EyQ0dHYnMzVDNWUXpu\nTm5Pc0VySFVHbW80ZFZlVzFJS0Z4TEtGYm04ClJDL2pZck5TTHpNbWpUcmlCaXV0\nNWs5MFlaUUM3c0s3YzVURVpLNXplRk0KLS0tIDEyd0p6Z3BWTTdXSVhSTEM1UFVO\nNkJwYmJDclhpMUdsKzRXQUZWYVFNZ0UKpIco6z8RdtVa5Rl+u2Swy6k/lhUUeT++\n79fOnHjPqNAnWEu+QRdcvmgjEyA1+1ZvH7+gT/uRZzwaZ0P0gSE4Cg==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-10-14T11:33:07Z",
+		"mac": "ENC[AES256_GCM,data:pcCaoic2ejZgrETVL3Qc40Cyd2yuSCu46Aha4TMWXlzVJxe3/YLdCY7xL7nJhXv+c9MxiXdyQJb1+ETIl9xJqPj6tRu5uHbC5ovr7PeRz6itt7ELyJtz/IFpjV0ElKzTjW53rgElZUv/52Vke71wHFfkcuIXgVozQSHZRJ+UR0E=,iv:+igw9g67P7G2VSbXhOwP1cUvTDlytUzxyHPegsFWiuM=,tag:caZHmf9WRmOk6HBOA0krNw==,type:str]",
+		"version": "3.11.0"
+	}
+}
