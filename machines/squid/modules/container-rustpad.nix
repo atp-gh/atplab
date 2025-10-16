75d954156dddb7a83c0f88dbd53cde14fe734eea
atp-gh <atp@disroot.org>
2025-10-16 22:26:08 +0800

    feat: Enable rustpad and add nginx basic auth

---
+++ machines/squid/modules/container-rustpad.nix
@@ -0,0 +1,45 @@
+{config, ...}: {
+  virtualisation.oci-containers.containers."rustpad" = {
+    image = "ekzhang/rustpad:latest";
+    environment = {
+      "EXPIRY_DAYS" = "1";
+      # "SQLITE_URI" = "";
+      # "PORT" = "3030";
+      # "RUST_LOG" = "data=debug,hardware=debug";
+    };
+    ports = [
+      "127.0.0.1:3030:3030"
+    ];
+  };
+  sops.secrets.squid-rustpad-basic-auth = {
+    mode = "0400";
+    owner = "nginx";
+    group = "nginx";
+    format = "binary";
+    sopsFile = ../secrets/rustpad-basic-auth;
+  };
+  services.nginx = {
+    virtualHosts = {
+      "pad.0pt.dpdns.org" = {
+        forceSSL = true;
+        kTLS = true;
+        sslCertificate = "/etc/nginx/self-sign.crt";
+        sslCertificateKey = "/etc/nginx/self-sign.key";
+        basicAuthFile = config.sops.secrets.squid-rustpad-basic-auth.path;
+        extraConfig = ''
+          proxy_hide_header X-Powered-By;
+          proxy_hide_header Server;
+        '';
+        locations = {
+          "/" = {
+            proxyPass = "http://127.0.0.1:3030";
+            recommendedProxySettings = true;
+            extraConfig = ''
+              proxy_buffering off;
+            '';
+          };
+        };
+      };
+    };
+  };
+}
