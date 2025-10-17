b6b3a9581293b8909bbc7f3b57162f54db8ecb56
atp-gh <atp@disroot.org>
2025-10-17 15:22:37 +0800

    feat: Enable openlist

---
+++ machines/homelab/modules/openlist.nix
@@ -0,0 +1,38 @@
+{config, ...}: let
+  cfg = config.services.openlist;
+in {
+  sops.secrets.homelab-openlist-env = {
+    mode = "0400";
+    owner = cfg.user;
+    group = cfg.group;
+    format = "binary";
+    sopsFile = ../secrets/openlist-env;
+  };
+  services = {
+    openlist = {
+      enable = true;
+      environmentFile = config.sops.secrets.homelab-openlist-env.path;
+    };
+    nginx.virtualHosts = {
+      "openlist.0pt.lab" = {
+        forceSSL = true;
+        kTLS = true;
+        sslCertificate = "/etc/nginx/self-sign.crt";
+        sslCertificateKey = "/etc/nginx/self-sign.key";
+        extraConfig = ''
+          proxy_hide_header X-Powered-By;
+          proxy_hide_header Server;
+        '';
+        locations = {
+          "/" = {
+            proxyPass = "http://127.0.0.1:5244";
+            recommendedProxySettings = true;
+            extraConfig = ''
+              proxy_buffering off;
+            '';
+          };
+        };
+      };
+    };
+  };
+}
