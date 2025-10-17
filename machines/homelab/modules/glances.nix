2a6db706743ca608101d36daa545ffabf6b75e22
atp-gh <atp@disroot.org>
2025-10-17 15:22:28 +0800

    feat: Add glances reverse proxy

--- machines/homelab/modules/glances.nix
+++ machines/homelab/modules/glances.nix
@@ -1,6 +1,33 @@
-_: {
-  services.glances = {
-    enable = true;
-    openFirewall = true;
+{config, ...}: let
+  cfg = config.services.glances;
+in {
+  services = {
+    glances = {
+      enable = true;
+      openFirewall = true;
+    };
+    nginx = {
+      virtualHosts = {
+        "glances.0pt.lab" = {
+          forceSSL = true;
+          kTLS = true;
+          sslCertificate = "/etc/nginx/self-sign.crt";
+          sslCertificateKey = "/etc/nginx/self-sign.key";
+          extraConfig = ''
+            proxy_hide_header X-Powered-By;
+            proxy_hide_header Server;
+          '';
+          locations = {
+            "/" = {
+              proxyPass = "http://127.0.0.1:${toString cfg.port}";
+              recommendedProxySettings = true;
+              extraConfig = ''
+                proxy_buffering off;
+              '';
+            };
+          };
+        };
+      };
+    };
   };
 }
