fc6a78cf791ac3abaf607ec5b5898d0494138ee3
atp-gh <atp@disroot.org>
2025-10-18 15:54:17 +0800

    feat: Enable readeck

---
+++ machines/squid/modules/container-readeck.nix
@@ -0,0 +1,30 @@
+_: {
+  virtualisation.oci-containers.containers."readeck" = {
+    image = "codeberg.org/readeck/readeck:latest";
+    volumes = [
+      "readeck:/readeck:rw"
+    ];
+    ports = [
+      "127.0.0.1:8001:8000"
+    ];
+  };
+  services.nginx.virtualHosts."readeck.0pt.dpdns.org" = {
+    forceSSL = true;
+    kTLS = true;
+    sslCertificate = "/etc/nginx/self-sign.crt";
+    sslCertificateKey = "/etc/nginx/self-sign.key";
+    extraConfig = ''
+      proxy_hide_header X-Powered-By;
+      proxy_hide_header Server;
+    '';
+    locations = {
+      "/" = {
+        proxyPass = "http://127.0.0.1:8001";
+        recommendedProxySettings = true;
+        extraConfig = ''
+          proxy_buffering off;
+        '';
+      };
+    };
+  };
+}
