48510ab20163cc3e69cf64d024d5d3620261a317
atp-gh <atp@disroot.org>
2025-10-18 16:34:59 +0800

    feat: Enable gotify on homelab

---
+++ machines/homelab/modules/gotify.nix
@@ -0,0 +1,32 @@
+{config, ...}: let
+  cfg = config.services.gotify;
+in {
+  services = {
+    gotify = {
+      enable = true;
+      environment = {
+        GOTIFY_SERVER_PORT = 1245;
+        GOTIFY_SERVER_LISTENADDR = "127.0.0.1";
+        GOTIFY_DATABASE_DIALECT = "sqlite3";
+        GOTIFY_REGISTRATION = "false";
+      };
+    };
+    nginx.virtualHosts."gotify.0pt.lab" = {
+      forceSSL = true;
+      kTLS = true;
+      sslCertificate = "/etc/nginx/self-sign.crt";
+      sslCertificateKey = "/etc/nginx/self-sign.key";
+      extraConfig = ''
+        proxy_hide_header X-Powered-By;
+        proxy_hide_header Server;
+      '';
+      locations."/" = {
+        proxyPass = "http://${toString cfg.environment.GOTIFY_SERVER_LISTENADDR}:${toString cfg.environment.GOTIFY_SERVER_PORT}";
+        recommendedProxySettings = true;
+        extraConfig = ''
+          proxy_buffering off;
+        '';
+      };
+    };
+  };
+}
