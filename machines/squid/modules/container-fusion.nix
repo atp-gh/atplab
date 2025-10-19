4c9440274d8fd1cebf1e60f390478a97d56a084e
atp-gh <atp@disroot.org>
2025-10-19 15:29:36 +0800

    feat: Enable fusion

---
+++ machines/squid/modules/container-fusion.nix
@@ -0,0 +1,36 @@
+{config, ...}: {
+  sops.secrets.squid-fusion-env = {
+    mode = "0400";
+    owner = "nginx";
+    group = "nginx";
+    format = "binary";
+    sopsFile = ../secrets/fusion-env;
+  };
+  virtualisation.oci-containers.containers."fusion" = {
+    image = "ghcr.io/0x2e/fusion:latest";
+    environmentFiles = [config.sops.secrets.squid-fusion-env.path];
+    volumes = [
+      "fusion:/data:rw"
+    ];
+    ports = [
+      "127.0.0.1:8081:8080"
+    ];
+  };
+  services.nginx.virtualHosts."fusion.0pt.dpdns.org" = {
+    forceSSL = true;
+    kTLS = true;
+    sslCertificate = "/etc/nginx/self-sign.crt";
+    sslCertificateKey = "/etc/nginx/self-sign.key";
+    extraConfig = ''
+      proxy_hide_header X-Powered-By;
+      proxy_hide_header Server;
+    '';
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:8081";
+      recommendedProxySettings = true;
+      extraConfig = ''
+        proxy_buffering off;
+      '';
+    };
+  };
+}
