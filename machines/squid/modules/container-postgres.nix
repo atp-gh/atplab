5c65b57cbd4516c90931ace416e5cfa3e2423984
atp-gh <atp@disroot.org>
2025-10-27 14:46:59 +0800

    feat: Enable postgres on squid

---
+++ machines/squid/modules/container-postgres.nix
@@ -0,0 +1,18 @@
+{config, ...}: {
+  sops.secrets.squid-postgres-env = {
+    mode = "0400";
+    format = "binary";
+    sopsFile = ../secrets/postgres-env;
+  };
+  virtualisation.oci-containers.containers."postgres" = {
+    pull = "newer";
+    image = "postgres:18-alpine";
+    environmentFiles = [config.sops.secrets.squid-postgres-env.path];
+    volumes = [
+      "postgres:/var/lib/postgresql/data:rw"
+    ];
+    ports = [
+      "127.0.0.1:5432:5432"
+    ];
+  };
+}
