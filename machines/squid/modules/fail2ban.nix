528a161f03e38a8814634c6e5a69850837c02516
atp-gh <atp@disroot.org>
2025-11-02 10:08:04 +0800

    chore: Make longer for fail2ban bantime

--- machines/squid/modules/fail2ban.nix
+++ machines/squid/modules/fail2ban.nix
@@ -27,12 +27,13 @@ in {
       maxretry = 5;
       ignoreIP = [
         (import ../values/ipv4-address1.nix)
+        (import ../values/dn42-peer-atp-octopus.nix)
       ];
-      bantime = "24h"; # Ban IPs for one day on the first ban
+      bantime = "300d"; # Ban IPs for one day on the first ban
       bantime-increment = {
         enable = true; # Enable increment of bantime after each violation
         multipliers = "1 2 4 8 16 32 64";
-        maxtime = "168h"; # Do not ban for more than 1 week
+        maxtime = "6000d"; # Do not ban for more than 1 week
         overalljails = true; # Calculate the bantime based on all the violations
       };
       jails = {
