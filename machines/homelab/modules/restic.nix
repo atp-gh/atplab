7684e93440addc3ea9dbb2aecabd128a88f71e61
atp-gh <atp@disroot.org>
2025-11-02 22:27:58 +0800

    feat: Enable restic on homelab

--- machines/homelab/modules/restic.nix
+++ machines/homelab/modules/restic.nix
@@ -1,38 +1,79 @@
-{config, ...}: {
-  #   sops.secrets.homelab-restic-remote1-password = {
-  #     mode = "0755";
-  #     format = "binary";
-  #     sopsFile = ../secrets/restic-remote1-password;
-  #   };
-  #   sops.secrets.homelab-restic-remote1-env = {
-  #     mode = "0755";
-  #     format = "binary";
-  #     sopsFile = ../secrets/restic-remote1-env;
-  #   };
-  #   sops.secrets.homelab-restic-remote1-repo = {
-  #     mode = "0755";
-  #     format = "binary";
-  #     sopsFile = ../secrets/restic-remote1-repo;
-  #   };
-  #   services.restic = {
-  #     backups = {
-  #       remote1 = {
-  #         initialize = false;
-  #         passwordFile = config.sops.secrets.homelab-restic-remote1-password.path;
-  #         environmentFile = config.sops.secrets.homelab-restic-remote1-env.path;
-  #         paths = [
-  #           "/var/lib/forgejo"
-  #           "/var/lib/homebox"
-  #           "/var/lib/openlist"
-  #           "/var/lib/postgresql"
-  #           "/var/lib/wakapi"
-  #         ];
-  #         repositoryFile = config.sops.secrets.homelab-restic-remote1-repo.path;
-  #         timerConfig = {
-  #           OnCalendar = "21:10:00";
-  #           Persistent = true;
-  #         };
-  #       };
-  #     };
-  #   };
+{
+  config,
+  hostname,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  grt = import ../values/gotify-restic-token.nix;
+in {
+  sops.secrets = let
+    secretNames = [
+      "restic-passwd"
+      "restic-remote1-env"
+      "restic-remote1-repo"
+    ];
+  in
+    builtins.listToAttrs (map (n: {
+        name = "homelab-${n}";
+        value = {
+          mode = "0400";
+          format = "binary";
+          sopsFile = ../secrets/${n};
+        };
+      })
+      secretNames);
+
+  services.restic = {
+    backups = let
+      common = {
+        initialize = true;
+        passwordFile = config.sops.secrets.homelab-restic-passwd.path;
+        paths = [
+          "/var/lib/forgejo"
+          "/var/lib/grafana"
+          "/var/lib/homebox"
+          "/var/lib/immich"
+          "/var/lib/openlist"
+          "/var/lib/postgresql"
+          "/var/lib/private/gotify-server"
+        ];
+        timerConfig.Persistent = true;
+      };
+
+      mkNotify = remote: {
+        prepare = ''
+          ${pkgs.curl}/bin/curl "http://127.0.0.1:1245/message?token=${grt}" \
+            -F "title=${hostname} restic ${remote} backup start" \
+            -F "message=$(date "+%H:%M:%S")" \
+            -F "priority=0"
+        '';
+        cleanup = ''
+          ${pkgs.curl}/bin/curl "http://127.0.0.1:1245/message?token=${grt}" \
+            -F "title=${hostname} restic ${remote} logs" \
+            -F "message=$(journalctl -u restic-backups-${remote}.service --since '10 minute ago' -o cat) | over." \
+            -F "priority=0"
+        '';
+      };
+
+      remotes = {
+        remote1 = {
+          repositoryFile = config.sops.secrets.homelab-restic-remote1-repo.path;
+          environmentFile = config.sops.secrets.homelab-restic-remote1-env.path;
+          timerConfig.OnCalendar = "22:26:00";
+        };
+      };
+
+      mkBackup = name: cfg:
+        recursiveUpdate common (
+          cfg
+          // {
+            backupPrepareCommand = (mkNotify name).prepare;
+            backupCleanupCommand = (mkNotify name).cleanup;
+          }
+        );
+    in
+      mapAttrs mkBackup remotes;
+  };
 }
---
+++ machines/homelab/values/gotify-restic-token.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:+81PpfVsAmznVQWYb0uCxQfN,iv:GnJ82SbKrd/IBlCjbW7JN09afj4UcO8RVob2CVMY91o=,tag:Q7+MfrXMQzv46S4pL88KgQ==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1gym933jnyln9nud8vhg44vtp983prgfpnlxr0zw2tr9x9s575vyqyufmyd",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSB0dWlUUFpqd0FQWXNZQkdj\nL2NFd1NTUFpQeVlwaUFvUE1pWGl5OWJBRnhnCjZZYnRtcnc4V3ZxMWJUcWJZcXVy\nRWRwYzEyY0FzNzN2djBnNDVvdE5sZkEKLS0tIENKcDZPT3pOMk5FMm53cmNWV3FW\nZzRXaXZ1K0dzbVYrOGpUdGVYbW5YeFUKuZpLdhgzqBgnsbEjTNv4wHJNubPnmHc3\nVRJQv2Lj4POQEGJICmzN3oIubH0tgQDt4D4OdLpP79SjmW/WqH0RJA==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-11-02T14:25:51Z",
+		"mac": "ENC[AES256_GCM,data:+093UJ9i9PNxAW4y3kWUtP6H3hvLerxtslrBpZRAm4jWdNy3LdZ2W2MDy5JUtQ7nSBCb6d2wTSYLw1GbczesC+EUwcStoJmKaiDv18uZdLwW1Suw6LbYHXYnql0r5uSic22k3SIQ/uBlncS28aAndPr+/yEBqd+fDoKs2mBQFyg=,iv:S3p87lWDg5fJa7MCzjEEuGIeITF49ax98LWp3pRKNXs=,tag:uMRue/Ztq1Utvd6dvWMsBg==,type:str]",
+		"version": "3.11.0"
+	}
+}
