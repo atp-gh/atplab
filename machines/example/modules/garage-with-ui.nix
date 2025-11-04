c73e33f786bbf1f184696d7c020c67171e960140
atp-gh <atp@disroot.org>
2025-11-04 20:34:55 +0800

    feat: Add garage to example

---
+++ machines/example/modules/garage-with-ui.nix
@@ -0,0 +1,47 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  cfg = config.services.garage;
+in {
+  services = {
+    garage = {
+      enable = true;
+      package = pkgs.garage_2;
+      settings = {
+        db_engine = "lmdb";
+        consistency_mode = "consistent";
+        replication_factor = 1;
+        compression_level = 2;
+        # Generate by `openssl rand -hex 32`
+        rpc_secret = "xxx";
+        rpc_bind_addr = "127.0.0.1:3901";
+        rpc_public_addr = "127.0.0.1:3901";
+        s3_api = {
+          api_bind_addr = "127.0.0.1:3900";
+          s3_region = "us-east-2";
+          root_domain = ".garage.example.com";
+        };
+        s3_web = {
+          bind_addr = "127.0.0.1:3902";
+          root_domain = ".garageweb.example.com";
+          index = "index.html";
+        };
+        admin = {
+          api_bind_addr = "127.0.0.1:3903";
+          # Generate by `openssl rand -base64 32`
+          admin_token = "xxx";
+          # Generate by `openssl rand -base64 32`
+          metrics_token = "xxx";
+        };
+      };
+    };
+    garage-webui = {
+      enable = true;
+      port = 3909;
+      apiBaseUrl = "http://${cfg.settings.admin.api_bind_addr}";
+      s3EndpointUrl = "http://${cfg.settings.s3_api.api_bind_addr}";
+    };
+  };
+}
--- machines/example/modules/garage.nix
+++ machines/example/modules/garage.nix
@@ -1,12 +1,16 @@
 {pkgs, ...}: {
   services.garage = {
     enable = true;
-    package = pkgs.garage;
+    package = pkgs.garage_2;
     settings = {
+      db_engine = "lmdb";
       consistency_mode = "consistent";
-      replication_factor = 3;
-      rpc_secret = "The5b8iHZzOGcr3o5QEkY6HsJdlSGEdCea18QV0j";
+      replication_factor = 1;
+      compression_level = 2;
+      # Generate by `openssl rand -hex 32`
+      rpc_secret = "xxx";
       rpc_bind_addr = "127.0.0.1:3901";
+      rpc_public_addr = "127.0.0.1:3901";
       s3_api = {
         api_bind_addr = "127.0.0.1:3900";
         s3_region = "us-west-2";
@@ -15,6 +19,14 @@
       s3_web = {
         bind_addr = "127.0.0.1:3902";
         root_domain = ".garageweb.example.com";
+        index = "index.html";
+      };
+      admin = {
+        api_bind_addr = "127.0.0.1:3903";
+        # Generate by `openssl rand -base64 32`
+        admin_token = "xxx";
+        # Generate by `openssl rand -base64 32`
+        metrics_token = "xxx";
       };
     };
   };
