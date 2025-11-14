3e778b5db8da0be7433c42d5840b4f511fce1e21
atp-gh <atp@disroot.org>
2025-11-14 12:42:31 +0800

    feat: Enable kvm module on homelab

--- machines/homelab/hardware.nix
+++ machines/homelab/hardware.nix
@@ -10,7 +10,7 @@
 
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme"];
   boot.initrd.kernelModules = [];
-  boot.kernelModules = [];
+  boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
 
   networking.useDHCP = lib.mkDefault true;
