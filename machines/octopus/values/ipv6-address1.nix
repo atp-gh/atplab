1ee320c66f5f7f49746630adb4f055d1c371b6f6
atp-gh <atp@disroot.org>
2025-11-26 10:57:52 +0800

    fix: Fix and enable ipv6 on octopus

--- machines/octopus/values/ipv6-address1.nix
+++ machines/octopus/values/ipv6-address1.nix
@@ -1,14 +1,14 @@
 {
-	"data": "ENC[AES256_GCM,data:2XzXJFk8mVWhULzVlKLj7e1seytoX8Y=,iv:7u3dYkxuaZxF9vlKWX3slttMRtTC92B4kWiMyndzjRM=,tag:q48iGr10Khhz8fA9InjhZA==,type:str]",
+	"data": "ENC[AES256_GCM,data:5t1TGO/Pxas5WH8I3bn6lU+Uxf9Vy31wXQ==,iv:TjuvlgRD8DxVL94JHb0jhQZ1y//RJrAE7VdqxZyncZc=,tag:u7546X/S+1oHJxAf7AEEuw==,type:str]",
 	"sops": {
 		"age": [
 			{
 				"recipient": "age1gym933jnyln9nud8vhg44vtp983prgfpnlxr0zw2tr9x9s575vyqyufmyd",
-				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSA0SmdiUi9zL0pTREN2akR0\nZFpreVd1ZUdUS2graDFFTzBVSVVuWWhkUkdnCnRzaHhQaktYMDlnWHc5ZFNqcEN5\nTnhYZE5DNmVhZ1AvSDF1ZUpzMXByeTAKLS0tIExOY2dpM3J1MmpRdU42cmVTRlBJ\naVl0UjMzSHJ5RndKM1UwWW1KNldkMEkKw3VIhDdhnv2kPQ4v1DiwU4e7XYYd7RgG\nOZyLQ+WgoJ46enju7S70ybiyOMlKIdtNQeNFgV9loJwPpcCZZntdnQ==\n-----END AGE ENCRYPTED FILE-----\n"
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBINU40eHJOUktid0NBSDEr\nN0p3N0hKWktPZEFRZDUzTEowQ3Z3T3ExVW1NClNKemFzZzhOUC9zb3FtMUZoNUV4\nZHVaV1RmMVVHdlllVE9SRW13bXNheFUKLS0tIGhjSk1YcHpUdTEwTXE1ODlwTncw\nU1ozNmlNc2FaeU1wYVIyUVZBZGhJL1EKp2dJ+RNtfrU1sQOOWl93yuUGla2C5sMi\nLB9qLBl/lkAeQpPxakW7+T1S/fnsTWRWrmb2gTEvz49KXvnu80yEIQ==\n-----END AGE ENCRYPTED FILE-----\n"
 			}
 		],
-		"lastmodified": "2025-10-12T05:31:52Z",
-		"mac": "ENC[AES256_GCM,data:oq6yJp/N99CdKwb6M/2R65GrFC3arfPZmAyP7xvptEoGNBUKsJd7lCCQ/oLelOE2c124Zlguhu3drQ4xJEIM42ceILRCGDJte7OMdM2+d0Q49bvK3cT2GkA+bpHTRwR1r2CZsd+i/9ES8DggP0hrMn2SNZI9VHQg1VPWdnGE0dI=,iv:YWQTNtLtd8laqsVxN8qGOw67penr2GTty/7lgQu+Nfk=,tag:nBYlsj45gehnuESdJY9uGA==,type:str]",
+		"lastmodified": "2025-11-26T02:29:42Z",
+		"mac": "ENC[AES256_GCM,data:L0JfXCDF28reTE1fqXLpXpjI9yn9exqSuMdAzEjAoGHo35t5/s1FvL2qEEjOwwoaZrhxBQvT2K2gLaXkSWV3p/P3xcmzi902thNAksikqY3PXx61UpDM099cSsAdQTJ/4Ibo9LL17tfIpxMI+sPXGyRK6OrmN4uNWDuwL7uMz2k=,iv:xY++oYhBDoIzybudT+pdi7ygQ7Muo1DWCfixilIIVOk=,tag:lIMjZUX/DfsslOxEjTgdEA==,type:str]",
 		"version": "3.11.0"
 	}
 }
--- machines/octopus/values/ipv6-gateway.nix
+++ machines/octopus/values/ipv6-gateway.nix
@@ -1,14 +1,14 @@
 {
-	"data": "ENC[AES256_GCM,data:O9yw2LvzOM0ejTua0CLQPYJj,iv:X3i0nGkN7SUhP8wtzEn2Ca6VpzxdPXjJYfgkYX5bMwQ=,tag:vPN8CzA9MSlJhjy+SfPyqA==,type:str]",
+	"data": "ENC[AES256_GCM,data:YAPuaWtLMCOT3fcAMgD+luzdlBI=,iv:h3ZF3BKiBetVWbkPxS5UfKyw7Rg0HtV18pUZx3n/gIw=,tag:vfZ/Nrxf7bRU2/A/Q53KZw==,type:str]",
 	"sops": {
 		"age": [
 			{
 				"recipient": "age1gym933jnyln9nud8vhg44vtp983prgfpnlxr0zw2tr9x9s575vyqyufmyd",
-				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBNcjF4Y05pUHlRRFF4NmFq\ndXhYRWxEdzJuMzN3Y00xbjFORkE4SkJpeTJjCmxkSmZYVjVZeGlhK25sNXBWSUNX\nbXhCT2hkR25pcENFY2ZTTWJkMThDa3cKLS0tIDVKeU0wNVJkRGtYdzlKYWNIM1NS\nWGJwQ1RFV0xFZTdxWEIzZGZROWdZYWMKY2uuLMie05h8VDHVrg6W/C+BGhw/W0kc\nxyDw5Nqda8V4ts0ThmPoFeHFLvV4Jcuaz5BgO4vF2yRaCw4qAqiF7A==\n-----END AGE ENCRYPTED FILE-----\n"
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBkQUNVbGp4NXpRR25lMnBQ\nSzBDWmJvNThWQURhQURsU3lHWk9DUXNiK1NvCk5KSmxuQVVDa3ozSWk4b0JHTGJC\nZDMydEQ4OUI0UW9NSWFHczVSZzNUckEKLS0tIDV1K1dRWlFvK0M1aWlWNThKSWE1\neVVZMExLcHZLSGV4Rm4vSDAwOXRFUmcKpkuXAemsEF3rxZjtyzSkYtJSU8bVIFlL\nhaoDC9ooYHZWzWbMeoX7xn8bgJ58svqlKhyZTyfmvBt70xKEIfN97g==\n-----END AGE ENCRYPTED FILE-----\n"
 			}
 		],
-		"lastmodified": "2025-10-12T05:31:52Z",
-		"mac": "ENC[AES256_GCM,data:4MPIBn/R7FFCQHT5GUqgL6J9kx30Dp2i20TwKmzNQumZU1nci/LUrR0ORTidEO8C3zBattJ8vQ//Zrqm6anCbPpJ0cRUZxrUI7v6weQVp0Qu4cLhXTCZk8LmQ2Pvl4Oq3W3CB+M971nXEzy09ludAsIAVEeIdYVozcxcSp6jBJQ=,iv:sWJ61K6/984dBi58RCeeF6LDi1hlimTq4p4uTV3kMow=,tag:5v7q9JsDXsevGEq8e0o1Mw==,type:str]",
+		"lastmodified": "2025-11-26T02:29:42Z",
+		"mac": "ENC[AES256_GCM,data:bZpz53Qu0vL1wwKyh4uq4+sToVOu7nPZBrEOIDAYVfT11gLXuyV6am14F+77BqyAMGd3BVB+aHs3TWQgB6M1GT9pdta3HTPIdO99VWS1Z4pr2p/qqA3wnZ4K1DuCfEMTtoGxbSkZxLfPnK5A/ib8EQ2GxgwTl8Yjkwhd0pneccg=,iv:qoNHLoW4dFZYiJ4nH/U9H5VyU9uKhLl77hjhUdnTUuA=,tag:p5L+vRyrwOY1yLhJ4z3+Xw==,type:str]",
 		"version": "3.11.0"
 	}
 }
