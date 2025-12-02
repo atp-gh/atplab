66613f959c3cbe6a488ecf4e29150f5e0362e335
atp-gh <atp@disroot.org>
2025-12-02 22:33:57 +0800

    chore: Allow openlist upload large file

--- machines/homelab/modules/openlist.nix
+++ machines/homelab/modules/openlist.nix
@@ -26,6 +26,7 @@ in {
         proxyPass = "http://127.0.0.1:5244";
         recommendedProxySettings = true;
         extraConfig = ''
+          client_max_body_size 10G;
           proxy_buffering off;
         '';
       };
