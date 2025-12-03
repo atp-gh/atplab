8535f0260b6c4ec0d47a2f3d4e2012726cd84c79
atp-gh <atp@disroot.org>
2025-12-03 15:28:39 +0800

    feat: Enable fail2ban on cthulhu and nautilus

---
+++ machines/cthulhu/values/gotify-fail2ban-token.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:Ux2ktNF0vCGPykYyTTP/4ne4,iv:7EPvP5g1QqtWRnNKrCwKVRuQZrWjvTRS5WRQ3Hl+8Iw=,tag:YOXfdjom7snugA1Tuq667A==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1gym933jnyln9nud8vhg44vtp983prgfpnlxr0zw2tr9x9s575vyqyufmyd",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBJU1hRVGVIeUMycThjcjBL\nNUJuTm8zY1VaeTBTVjI3b2NabDJLVkRnNEVjCmc0eFg3UUduVy81aStpbFkzQ3FM\ndDNzU2ZXNDd2RDQwL0ZwTC9PRFQzb1EKLS0tIHEwbmUwV213RTlkaDlmdTFWUmVS\ncTQ2L2QyZGxzSDV6dVBoVlNvSFpuRDAKLRwpymLByw7bntMxADOIyWmpSYbFNisB\nC790Mz4+eCk3yy8u3SDUSBiDIbQd6y36LbEGNjw9Ejkkp0g1Ak1+5g==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-12-03T07:22:07Z",
+		"mac": "ENC[AES256_GCM,data:iwKBvoQtxFcH6X8LnrlQVNLtDQltEsxmUrn0UwsoWi0d6h8qIaXsuDwuKUM6x+9p0gMS/pLNjASun3yGwl3rX7f0OpznZ7vYGJnl3v74jCf3aEKCDk7MCA4t75uzadRnFeJCr0lHJW2+QQvrdLzq1CWuePpqiCj1HP+1tNtyTw4=,iv:FhrwEs2BxPcuZjE7iT8V8cFaqurgn7zrKg+kikD0eb4=,tag:dLQJMY8cEm+BFKgwKd8Yng==,type:str]",
+		"version": "3.11.0"
+	}
+}
---
+++ machines/nautilus/values/gotify-fail2ban-token.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:eMbH1tOAPcdYFdHVCC+CwYYM,iv:QJ8WNxUif19cnvkKWDKxE6V+UCnwHbNDQP6WrwPEsKI=,tag:2cUE8em+fs8/zkH8IIzuXA==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1gym933jnyln9nud8vhg44vtp983prgfpnlxr0zw2tr9x9s575vyqyufmyd",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBqVjR5VUVITlJpM0lBM0x2\nYkRXNHlKV2FxQnVHNzM5VG5ZWi94S2NFNFJRClJpV010SjY4TGp4UVEwSkxvTmky\nL0xDbmQzcmkrQllDU3pobDdCNnFIWTAKLS0tIDRpdUxLMlpuUUduY0ErQU4zZGd1\nWnRkcTBRbFB2T3JLc1NIN2RNZEdwWU0Ko4qK9ItEPGb+5nNXQ8Z7rmR8sv5bDFwG\nM2d0aoUSk2vMZxvZFXgLx1eD+WpetqPw1NICw1ezgNYgkcN8qLl8NQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-12-03T07:23:16Z",
+		"mac": "ENC[AES256_GCM,data:tetF1+BZb0DnPjMflSU3k+CwHBYRRNKNX9QbvjHTtkBuXFSvHCgMcP1sBSD4M7mmzAIEmACwmBcGMGBqtlFFrfWMtm0mHNc0HtJy/GSHf9DqFo1iOKFLxNmqX5oLyMmey4krJufDMeAB6vKYCLQXVl8GaKIsTIxwrom9IcZYpYg=,iv:TugzNCHPNg2IdDSZ/fOZ5mOfHxFYJQhyzD9Y6Ai5ivU=,tag:8XBNau/tjVE7+dKJcyoLgA==,type:str]",
+		"version": "3.11.0"
+	}
+}
