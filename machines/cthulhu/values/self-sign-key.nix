67f266173de81f611c6c025fa799477febfe11a1
atp-gh <atp@disroot.org>
2025-12-05 20:24:02 +0800

    feat: Enable nginx on cthulhu

---
+++ machines/cthulhu/values/self-sign-key.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:YR1Dky4NleBADRo5OWpbf/SyHW0NnhBl0D/C355pjlZ4bkvMllskaJagVVWUyEktut0EBVkTvJ+e9mqIqlKgp3+1UTZkzo7ibXGoYTqKwbAgzxQ2Lhn83oqXydVHsCJzOBaHnwFh904SophEN8e0APTSe9gsj5BWSajt8QRxtOnZodtBb6ItpfohlBJw4wWHLXICgPDTQr3Z32gAHA81HeHQ6mlJwpU5r4ihfT9UtUlz2idqwclYwbvtKxV55UIpwHbM3fowz/8hrrE3mxiSwO8ix486gEkfElbIhDacOEG976pfrvDPsDUVQsCHn5xhTsAwPb9GjDjpA4vZuoNc7qb2EVuXHYgSB6FrUgzkvXmM/bcTj43YQndKX2t2kAfnUH4LPVrU9yBonV4iSSdIX8JDKAOB2jcfudNebCdDNvLO17A2,iv:r2KiyqnVhLLDJQkaWuUCgnr0P/llDSS6DKnd0gxg7Mg=,tag:1V9HsNiLw/AKFbg6PAXHLw==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1gym933jnyln9nud8vhg44vtp983prgfpnlxr0zw2tr9x9s575vyqyufmyd",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBrSElybUZxcklBK1dRN2o5\nZWo1d21velE2M2RUelpIcWc1VGswVlIxMzNJCjZFcTZJVlpqTkJhQU92ZlI1dUhh\nUnFWZ1prek9kRkJLaWxNU2l1VjBEdmsKLS0tIEw3NW9LQ3BpYWxORE8vOUVzVGd0\nY3BIZ1I0RnMvd0pkZmszQVVQZHBJLzQKV5BHmOEsJ9TNe3j13LzUlJBeH+XigHQm\nQTkujYgHEuiP+fkiz5kCd4fYucTb9X13D3v0zheA2VDpgbaEc8c4Qw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-12-05T12:14:05Z",
+		"mac": "ENC[AES256_GCM,data:5PKcnRusL//Fjdgum9lBJgKlKVvr+dVKHRbDL3TlGkODxtpNza0KlTpKWvmjp1kYFqfJAXn8nb5VmynA5a8sQhe+j9Lrx6oQns+eymAyQsftBk4j9In4wVLJwDyDdn7mW8pRkhMs41mxtaOAIZCg1dXT93yo2hTWChNWSe83fhY=,iv:rwaxOp2TOWU9+Oo0oNkPHQLHe8NhGqQmAEJ0b7geIjs=,tag:skaeLyLqC1RROVH5ZZqGzA==,type:str]",
+		"version": "3.11.0"
+	}
+}
