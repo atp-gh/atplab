e09df22e6c2365603b5bc098751930d467f673d3
atp-gh <atp@disroot.org>
2025-12-06 22:19:08 +0800

    feat: Enable restic on nautilus

---
+++ machines/nautilus/modules/restic.nix
@@ -0,0 +1,97 @@
+{
+  config,
+  hostname,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  grt = import ../values/gotify-restic-token.nix;
+in {
+  sops.secrets = let
+    secretNames = [
+      "restic-passwd"
+      "restic-remote1-env"
+      "restic-remote1-repo"
+      "restic-remote2-rclone"
+      "restic-remote2-repo"
+      "restic-remote3-env"
+      "restic-remote3-repo"
+      "restic-remote4-env"
+      "restic-remote4-repo"
+    ];
+  in
+    builtins.listToAttrs (map (n: {
+        name = "nautilus-${n}";
+        value = {
+          mode = "0400";
+          format = "binary";
+          sopsFile = ../secrets/${n};
+        };
+      })
+      secretNames);
+
+  services.restic = {
+    backups = let
+      common = {
+        initialize = true;
+        passwordFile = config.sops.secrets.nautilus-restic-passwd.path;
+        paths = [
+          "/var/lib/komari-server"
+          "/var/lib/private/gatus"
+          "/var/lib/private/gotify-server"
+          "/var/lib/private/uptime-kuma"
+        ];
+        timerConfig.Persistent = true;
+      };
+
+      mkNotify = remote: {
+        prepare = ''
+          ${pkgs.curl}/bin/curl "http://${config.services.gotify.environment.GOTIFY_SERVER_LISTENADDR}:${toString config.services.gotify.environment.GOTIFY_SERVER_PORT}/message?token=${grt}" \
+            -F "title=${hostname} restic ${remote} backup start" \
+            -F "message=$(date "+%H:%M:%S")" \
+            -F "priority=0"
+        '';
+        cleanup = ''
+          ${pkgs.curl}/bin/curl "http://${config.services.gotify.environment.GOTIFY_SERVER_LISTENADDR}:${toString config.services.gotify.environment.GOTIFY_SERVER_PORT}/message?token=${grt}" \
+            -F "title=${hostname} restic ${remote} logs" \
+            -F "message=$(journalctl -u restic-backups-${remote}.service --since '10 minute ago' -o cat) | over." \
+            -F "priority=0"
+        '';
+      };
+
+      remotes = {
+        remote1 = {
+          repositoryFile = config.sops.secrets.nautilus-restic-remote1-repo.path;
+          environmentFile = config.sops.secrets.nautilus-restic-remote1-env.path;
+          timerConfig.OnCalendar = "01:00:00";
+        };
+        remote2 = {
+          repositoryFile = config.sops.secrets.nautilus-restic-remote2-repo.path;
+          rcloneConfigFile = config.sops.secrets.nautilus-restic-remote2-rclone.path;
+          timerConfig.OnCalendar = "01:10:00";
+        };
+        remote3 = {
+          repositoryFile = config.sops.secrets.nautilus-restic-remote3-repo.path;
+          environmentFile = config.sops.secrets.nautilus-restic-remote3-env.path;
+          timerConfig.OnCalendar = "01:20:00";
+        };
+        remote4 = {
+          repositoryFile = config.sops.secrets.nautilus-restic-remote4-repo.path;
+          environmentFile = config.sops.secrets.nautilus-restic-remote4-env.path;
+          timerConfig.OnCalendar = "01:30:00";
+        };
+      };
+
+      mkBackup = name: cfg:
+        recursiveUpdate common (
+          cfg
+          // {
+            backupPrepareCommand = (mkNotify name).prepare;
+            backupCleanupCommand = (mkNotify name).cleanup;
+          }
+        );
+    in
+      mapAttrs mkBackup remotes;
+  };
+}
---
+++ machines/nautilus/values/gotify-restic-token.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:AiYcBMLqwFcSNtH+6FTyzlt7,iv:Wd6c6DtSWQ3zwemiPDT1MDXjYYgaJZ+8ZPRBiLSlwXg=,tag:DeI916FR34kENYA2f55Viw==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1gym933jnyln9nud8vhg44vtp983prgfpnlxr0zw2tr9x9s575vyqyufmyd",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSByMFJjamplbkIyTUlTbEF2\nYXFyLzQzSHdDaUkyUUN0U2ZmazU2b0xnSGxJClZvZDlFbGhqSlJERUh4R1lPSmx0\nTDRCOERQOTl5QUZYSUEvVEpuRmZXK2MKLS0tIFhzTTk1cC9ORHNsdFpua0lvSEV4\nSzZnaGJxcUJhdjU2TDg4ZHlhRTlwVEkKxcVQ5Mh/1UtSNdlMr42XiBpRzKQW/bJl\nZ9ApmVo2j9dsDHYZ7FfZ4jQEZpWcJqv8rnODb+6K7hDvm4KveR1HzA==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-12-06T14:17:13Z",
+		"mac": "ENC[AES256_GCM,data:jbAvUMH3XMNz0E0KAWVMwwKfPfcxc1AkERct8VGdsFrCKbDFWApXvZJdAmjlt7qnDR0nkBNBr9kG7Ze3bDPX/ieSo3kamkrfAO//WWlt2VkhVTGRhdMZ5PaCVOfwux2zVCYeu4TSNG2G1b5R1a/QDkGfS+/gXCDEmhyiZxgZ/WM=,iv:pI+K4TApwoE7uotFS7t8Wwm5NwUOyURrmr61fvE/fSI=,tag:VWTQoQrT69IkElOnNul+qQ==,type:str]",
+		"version": "3.11.0"
+	}
+}
