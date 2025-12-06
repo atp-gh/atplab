dbb810eb8fe8a2c68a0a715fb9363d0954d5905a
atp-gh <atp@disroot.org>
2025-12-06 16:10:24 +0800

    feat: Enable nginx and anubis on nautilus

---
+++ machines/nautilus/values/self-sign-key.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:1ijiSRk6oJ6t9/i1czUdWnBwLFeEKuWLb3P75f5YjWJ0+b5FIcrPDTB1OYR3nx92TWTB8kbVRA9NNTucfANO/FNRHmT/bhgPHXObmHd2/8N6r7LPtn7xmIrHqemp+jzhiBbMyxGScjPZd1MObbu+v0jtPWugH/wXjed54HaWrGsg5/d17KLDCw9d3ezyZGurAfhpJJWj2rSdl2rYC1g/wsEDHy1Uan9hOlG1epDBPZfAxRHhBhmjy7ott/tV3qCRa0jUpDesnPe2LsczqSg4//jiLetSCYAdW5Ht+ay29VIeNgC06fHlqzJnrIN8sWlJYp6EghzVk+9KHSO5jrnRb3x/9Gv9rpeLtS14kn4hJPpQrB3KsKyDVABE/rqyOzpBEWkUJq/VewEXqQBK7tZ3iAoKjIA2KYyQ0jPlOdBEGvafx5Cs,iv:BtzY3YsX1b01W9Clm//hoKyuHRHcub/AA2sS5YiungA=,tag:rlOlsUqfdnurDRBdCQT4Yg==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1gym933jnyln9nud8vhg44vtp983prgfpnlxr0zw2tr9x9s575vyqyufmyd",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBHd0l3RTJGbUlzQ3ZjZjZi\nWDF0ckhpYVNHVDJUWE9sZ2tnYVdlU1BDaTB3Cng3V05iNk02TVBMeTFrZzJ5eEhq\ndVRHb1JselpPSEtITDlXaXd3VU54YjgKLS0tIE5SR0g5MG94b29XRTJsTEhlNGVN\nUmxzL3BsNnFZY3p3WnQvSE9rYzFZdDAKjqJoTTO4G8Z3bveyof1fYRncYbncAz2h\n7S1wUVJjQfTArUbM9PVBcWOgbZRiJFrUaPJYZHqs3RJK4Rf30tc8fw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-12-06T08:06:27Z",
+		"mac": "ENC[AES256_GCM,data:TK5+PLy2EIQ2ZiNzkNX3oEp70EPFioSa8ykIFyPARUr2KPtZl+Z23rbm+bKKkEYyXtGxLswoeh18CE/I05mBihUXbUyPXwuVbxXZFnIGhRURqpKCQJEVCtQQuf83zMtJGmr0LrTYhZ7Nult7dJz13MFPW9igi8Wt8VFo9ct5vrM=,iv:pp+pEKF9hTREJ2AVDyOfi5YjOTmNqi3UmDzdaEfEgx4=,tag:UOclZiN9HB0hs75N44sdPA==,type:str]",
+		"version": "3.11.0"
+	}
+}
