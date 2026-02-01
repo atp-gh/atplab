a65362db8817119bd448997ea8dd61d42c0b61a1
atp-gh <atp@disroot.org>
2026-02-01 22:22:44 +0800

    test(ok): Test kanidm as oidc provider

--- machines/octopus/modules/kanidm.nix
+++ machines/octopus/modules/kanidm.nix
@@ -5,9 +5,32 @@
 }: let
   cfg = config.services.kanidm;
 in {
+  sops.secrets = {
+    octopus-kanidm-admin = {
+      mode = "0400";
+      owner = "kanidm";
+      group = "kanidm";
+      format = "binary";
+      sopsFile = ../secrets/kanidm-admin;
+    };
+    octopus-kanidm-idmadmin = {
+      mode = "0400";
+      owner = "kanidm";
+      group = "kanidm";
+      format = "binary";
+      sopsFile = ../secrets/kanidm-idmadmin;
+    };
+    octopus-kanidm-miniflux-bs = {
+      mode = "0444";
+      owner = "kanidm";
+      group = "kanidm";
+      format = "binary";
+      sopsFile = ../secrets/kanidm-miniflux-bs;
+    };
+  };
   services = {
     kanidm = {
-      package = pkgs.kanidm_1_8;
+      package = pkgs.kanidm_1_8.withSecretProvisioning;
       enableServer = true;
       serverSettings = {
         bindaddress = "127.0.0.1:8004";
@@ -17,7 +40,41 @@ in {
         tls_key = "/etc/nginx/self-sign.key";
       };
       enableClient = true;
-      clientSettings.uri = "${cfg.serverSettings.bindaddress}";
+      clientSettings.uri = "https://${cfg.serverSettings.domain}";
+      provision = {
+        enable = true;
+        adminPasswordFile = config.sops.secrets.octopus-kanidm-admin.path;
+        idmAdminPasswordFile = config.sops.secrets.octopus-kanidm-idmadmin.path;
+        persons = {
+          test = {
+            displayName = "My Test";
+            legalName = "Test Test";
+            mailAddresses = ["test@example.com"];
+            # groups = ["testgp"];
+          };
+        };
+        groups = {
+          testgp = {
+            members = ["test"];
+          };
+        };
+        systems.oauth2 = {
+          miniflux = {
+            displayName = "Miniflux";
+            basicSecretFile = config.sops.secrets.octopus-kanidm-miniflux-bs.path;
+            originUrl = "https://test.0pt.dpdns.org/oauth2/oidc/callback";
+            originLanding = "https://test.0pt.dpdns.org";
+            preferShortUsername = true;
+            scopeMaps = {
+              testgp = [
+                "openid"
+                "email"
+                "profile"
+              ];
+            };
+          };
+        };
+      };
     };
     nginx.virtualHosts."${cfg.serverSettings.domain}" = {
       forceSSL = true;
---
+++ machines/octopus/modules/miniflux.nix
@@ -0,0 +1,50 @@
+{config, ...}: let
+  cfg = config.services.miniflux;
+in {
+  services = {
+    miniflux = {
+      enable = true;
+      config = {
+        CREATE_ADMIN = false;
+        LISTEN_ADDR = "127.0.0.1:8005";
+        WATCHDOG = false;
+        OAUTH2_PROVIDER = "oidc";
+        OAUTH2_OIDC_PROVIDER_NAME = "Kanidm";
+        OAUTH2_CLIENT_ID = "miniflux";
+        OAUTH2_CLIENT_SECRET_FILE = config.sops.secrets.octopus-kanidm-miniflux-bs.path;
+        OAUTH2_REDIRECT_URL = "https://test.0pt.dpdns.org/oauth2/oidc/callback";
+        OAUTH2_OIDC_DISCOVERY_ENDPOINT = "https://kanidm.0pt.dpdns.org/oauth2/openid/miniflux";
+        OAUTH2_USER_CREATION = 1; # For OIDC Register
+      };
+    };
+    nginx.virtualHosts."test.0pt.dpdns.org" = {
+      forceSSL = true;
+      kTLS = true;
+      sslCertificate = "/etc/nginx/self-sign.crt";
+      sslCertificateKey = "/etc/nginx/self-sign.key";
+      extraConfig = ''
+        proxy_hide_header X-Powered-By;
+        proxy_hide_header Server;
+      '';
+      locations."/" = {
+        proxyPass = "http://unix:${config.services.anubis.instances.miniflux.settings.BIND}:";
+        recommendedProxySettings = true;
+        extraConfig = ''
+          proxy_buffering off;
+          client_max_body_size 20000m;
+        '';
+      };
+    };
+    anubis.instances.miniflux.settings = {
+      TARGET = "http://${cfg.config.LISTEN_ADDR}";
+      BIND = "/run/anubis/anubis-miniflux/anubis-miniflux.sock";
+      METRICS_BIND = "/run/anubis/anubis-miniflux/anubis-miniflux-metrics.sock";
+    };
+  };
+  users.users.miniflux = {
+    name = "miniflux";
+    group = "miniflux";
+    isSystemUser = true;
+  };
+  users.groups.miniflux = {};
+}
