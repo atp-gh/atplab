e7478ae6a0609d0b52aed6e826d653d3218c6f72
atp-gh <atp@disroot.org>
2026-02-06 18:08:09 +0800

    chore: Import zfs config

--- machines/icecream/default.nix
+++ machines/icecream/default.nix
@@ -7,6 +7,8 @@ in {
       ./disko.nix
       ./hardware.nix
       ./user.nix
+
+      ../../modules/services/zfs.nix
     ]
     ++ ls ./modules
     ++ ls ../../modules/system
@@ -16,4 +18,6 @@ in {
   disko.devices.disk.main.device = primary-disk;
   system.stateVersion = "26.05";
   networking.hostId = "2896c4e1";
+  boot.kernelParams = ["zfs.zfs_arc_max=4294967296"]; # Zfs arc is a beast, it would eat out all memory if you don't set limit!
+  boot.zfs.devNodes = "/dev/disk/by-id";
 }
