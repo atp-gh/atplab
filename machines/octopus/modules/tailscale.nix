98c78c39e653846b30c24012c2901aac9b596ed8
atp-gh <atp@disroot.org>
2026-02-22 19:04:00 +0800

    test: Shut down tailscale to test dn42

--- machines/octopus/modules/tailscale.nix
+++ machines/octopus/modules/tailscale.nix
@@ -2,7 +2,7 @@
   cfg = config.services.tailscale;
 in {
   services.tailscale = {
-    enable = true;
+    enable = false;
     openFirewall = true;
     useRoutingFeatures = "both";
     extraSetFlags = ["--accept-dns=false"];
