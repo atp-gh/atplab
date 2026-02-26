b49c56fe5b14d1d473eaa2dd7a217df96b6ded6d
atp-gh <atp@disroot.org>
2026-02-26 20:58:43 +0800

    chore: Update komari-server package

--- pkgs/komari-server/default.nix
+++ pkgs/komari-server/default.nix
@@ -6,10 +6,10 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "komari-server";
-  version = "1.1.4";
+  version = "1.1.7";
   src = fetchurl {
-    url = "https://github.com/komari-monitor/komari/releases/download/1.1.4/komari-linux-amd64";
-    sha256 = "sha256-v/I3l44F3ruZiyFtwNxuLHf/3ddt73lwPy1utTmrvnI=";
+    url = "https://github.com/komari-monitor/komari/releases/download/1.1.7/komari-linux-amd64";
+    sha256 = "sha256-I3AvMThO4md/7S6OYRIuYL6lOL2bMKmZz0A828L4l54=";
   };
   dontUnpack = true;
   installPhase = ''
