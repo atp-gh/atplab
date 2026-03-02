c3ae0b88297defa8aa6e9dbfcb48dbde8cf0d3c2
atp-gh <atp@disroot.org>
2026-03-02 21:08:35 +0800

    feat: Enable restic on icecream

---
+++ machines/icecream/modules/restic.nix
@@ -0,0 +1,89 @@
+{
+  config,
+  hostname,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  grt = import ../values/gotify-restic-token.nix;
+in {
+  sops.secrets = let
+    secretNames = [
+      "restic-passwd"
+      "restic-remote1-env"
+      "restic-remote1-repo"
+      "restic-remote2-repo"
+    ];
+  in
+    builtins.listToAttrs (map (n: {
+        name = "${hostname}-${n}";
+        value = {
+          mode = "0400";
+          format = "binary";
+          sopsFile = ../secrets/${n};
+        };
+      })
+      secretNames);
+
+  services.restic = {
+    backups = let
+      common = {
+        initialize = true;
+        passwordFile = config.sops.secrets."${hostname}-restic-passwd".path;
+        paths = [
+          "/var/lib/forgejo"
+          "/var/lib/grafana"
+          "/var/lib/homebox"
+          "/var/lib/immich"
+          "/var/lib/openlist"
+          "/var/lib/postgresql"
+          "/var/lib/vigil-server"
+          "/var/lib/private/gotify-server"
+        ];
+        timerConfig.Persistent = true;
+        pruneOpts = [
+          "--keep-daily 4"
+          "--group-by host"
+        ];
+      };
+
+      mkNotify = remote: {
+        prepare = ''
+          ${pkgs.curl}/bin/curl "http://127.0.0.1:1245/message?token=${grt}" \
+            -F "title=${hostname} restic ${remote} backup start" \
+            -F "message=$(date "+%H:%M:%S")" \
+            -F "priority=0"
+        '';
+        cleanup = ''
+          ${pkgs.curl}/bin/curl "http://127.0.0.1:1245/message?token=${grt}" \
+            -F "title=${hostname} restic ${remote} logs" \
+            -F "message=$(journalctl -u restic-backups-${remote}.service --since '10 minute ago' -o cat) | over." \
+            -F "priority=0"
+        '';
+      };
+
+      remotes = {
+        remote1 = {
+          repositoryFile = config.sops.secrets."${hostname}-restic-remote1-repo".path;
+          environmentFile = config.sops.secrets."${hostname}-restic-remote1-env".path;
+          timerConfig.OnCalendar = "22:26:00";
+        };
+        remote2 = {
+          repositoryFile = config.sops.secrets."${hostname}-restic-remote2-repo".path;
+          timerConfig.OnCalendar = "21:00:00";
+        };
+      };
+
+      mkBackup = name: cfg:
+        recursiveUpdate common (
+          cfg
+          // {
+            backupPrepareCommand = (mkNotify name).prepare;
+            backupCleanupCommand = (mkNotify name).cleanup;
+          }
+        );
+    in
+      mapAttrs mkBackup remotes;
+  };
+}
---
+++ machines/icecream/values/gotify-restic-token.nix
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:QuyXJC9pq1XCGPMPfUYWPo7O,iv:FbJXE1UQT+DjHUjkbChnQxa+t0eq+h7TiZp8dOjQtg8=,tag:mP9QeD/imizagJQG7wMXUg==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1gym933jnyln9nud8vhg44vtp983prgfpnlxr0zw2tr9x9s575vyqyufmyd",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSB1bnQvMHFWaU5WSURab1lH\nQmh6QzkwSDliUlRycVpNMHdjN29jSkNCemdzCkttLzdwTHVINUxhbmtFN1ZvS0RY\nZTIxUEhsblcwSkdUanhLNXk4M2srV2MKLS0tIGRKY1Q3TndRUzVWQWVQYmNGQ2FR\nb0hJdGZBZnBxRUMxeldUdG5kMzBCQm8KasqohX2OQzJWsb93foIggttE4F9wNb75\n168qE0JW8/G7Djf2eZ6U5I+2tj/USmmd8Ir/CWD8KIleQy1SMH/SsA==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2026-03-02T12:36:16Z",
+		"mac": "ENC[AES256_GCM,data:wuE0VF3fX9OcAER8m3Sw/vO0XUsyNtW99xNUNFyXK2Ytx4k6Zy4zhNHVE+7PlVO7G9QU4FZoaGEuoqP1O85s4jk1W9zeRnmlDFBahEiveZfjsms0U4c+ZDrfm7EtQzvWV6VEmEO5blwz+N6vDcFx+WDr19zpZ+U0cKWPka54qaQ=,iv:RvKBVXiewCfYMXr4dp5ZD9NWf6bdx2skxRj3E3xi9dQ=,tag:xycT+6g/cfiEcy+cvBrYqA==,type:str]",
+		"version": "3.11.0"
+	}
+}
