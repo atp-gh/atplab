46f9ead676f5b9e78478aaa7bc82bdc807d643f2
atp-gh <atp@disroot.org>
2026-03-03 14:28:25 +0800

    chore: Enable syncthing

---
+++ machines/icecream/modules/syncthing.nix
@@ -0,0 +1,10 @@
+_: {
+  services = {
+    syncthing = {
+      enable = true;
+      openDefaultPorts = true;
+      guiAddress = "0.0.0.0:8384";
+    };
+  };
+  networking.firewall.allowedTCPPorts = [8384];
+}
