5610b07f3bda2eafa5e551b63c7add4c292b9820
atp-gh <atp@disroot.org>
2026-03-03 14:29:58 +0800

    feat: Improved the application of authoritative DNS servers with atp.dn42

--- machines/cthulhu/modules/knot.nix
+++ machines/cthulhu/modules/knot.nix
@@ -1,4 +1,6 @@
-_: {
+_: let
+  dnsSerial = "2026030201";
+in {
   services.knot = {
     enable = true;
     settings = {
@@ -8,16 +10,39 @@ _: {
           "fd25:5547:5a89::3@53"
         ];
       };
+      policy = [
+        {
+          id = "dn42-dnssec";
+          algorithm = "ed25519";
+          ksk-lifetime = "3650d";
+          zsk-lifetime = "30d";
+          dnskey-ttl = "1h";
+          rrsig-lifetime = "14d";
+          nsec3 = false;
+        }
+      ];
       template = [
         {
           id = "dn42";
           storage = "/etc/knot/dn42";
-          file = "atp.dn42.zone";
+          dnssec-signing = true;
+          dnssec-policy = "dn42-dnssec";
         }
       ];
       zone = [
         {
           domain = "atp.dn42";
+          file = "atp.dn42.zone";
+          template = "dn42";
+        }
+        {
+          domain = "0/28.192.20.172.in-addr.arpa";
+          file = "dn42.v4.rev.zone";
+          template = "dn42";
+        }
+        {
+          domain = "9.8.a.5.7.4.5.5.5.2.d.f.ip6.arpa";
+          file = "dn42.v6.rev.zone";
           template = "dn42";
         }
       ];
@@ -33,7 +58,7 @@ _: {
         $TTL 1h
 
         @   IN SOA ns1.atp.dn42. hostmaster.atp.dn42. (
-                2025012001 ; serial
+                ${dnsSerial} ; serial
                 1h         ; refresh
                 15m        ; retry
                 30d        ; expire
@@ -44,6 +69,49 @@ _: {
 
         ns1 IN A     172.20.192.3
         ns1 IN AAAA  fd25:5547:5a89::3
+      '';
+    };
+    "knot/dn42/dn42.v4.rev.zone" = {
+      user = "knot";
+      group = "knot";
+      mode = "0400";
+      text = ''
+        $ORIGIN 0/28.192.20.172.in-addr.arpa.
+        $TTL 1h
+
+        @ IN SOA ns1.atp.dn42. hostmaster.atp.dn42. (
+            ${dnsSerial}
+            1h
+            15m
+            30d
+            2h
+        )
+
+            IN NS ns1.atp.dn42.
+
+        3   IN PTR ns1.atp.dn42.
+      '';
+    };
+    "knot/dn42/dn42.v6.rev.zone" = {
+      user = "knot";
+      group = "knot";
+      mode = "0400";
+      text = ''
+        $ORIGIN 9.8.a.5.7.4.5.5.5.2.d.f.ip6.arpa.
+        $TTL 1h
+
+        @ IN SOA ns1.atp.dn42. hostmaster.atp.dn42. (
+            ${dnsSerial}
+            1h
+            15m
+            30d
+            2h
+        )
+
+            IN NS ns1.atp.dn42.
+
+        ; fd25:5547:5a89::3
+        3.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0 IN PTR ns1.atp.dn42.
       '';
     };
   };
