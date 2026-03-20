b5fcf328de204c8acb5f4d069b3acf2b771c4221
atp-gh <atp@disroot.org>
2026-03-20 23:23:24 +0800

    chore: Update flake.lock in 2026-03-20

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     daeuniverse.url = "github:daeuniverse/flake.nix";
   };
   outputs = inputs: let
-    hostname = "example";
+    hostname = "ammonite";
   in
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux"];
